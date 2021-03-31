5f6b5e542976f66f918c05521565efd45b12fa13
Mateusz Ożóg <mateusz2345678@gmail.com>
2021-03-31 09:51:45 +0200

    :+1: Card styling

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -4,7 +4,14 @@ const Home: React.FC = () => {
 	return (
 		<>
 			<div className="home h-full d-flex align-items-center justify-content-center">
-				<i className="bx bx-coffee display-1 text-light"></i>
+				<div className="card shadow-lg w-100">
+					<div className="card-header">
+						<h2 className="mb-0">Hello</h2>
+					</div>
+					<div className="card-body">
+						<p className="mb-0">123</p>
+					</div>
+				</div>
 			</div>
 		</>
 	);
