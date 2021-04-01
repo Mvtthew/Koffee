e623d09c29d803449f5df638c26ff8311162b2cd
Mateusz Ożóg <mateusz2345678@gmail.com>
2021-04-01 12:03:43 +0200

    :+1: Cleanup

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
+import React from "react";
 import { RootState } from "../../store";
 import { addCoffee } from "../../store/coffee/cofeeStore";
 
